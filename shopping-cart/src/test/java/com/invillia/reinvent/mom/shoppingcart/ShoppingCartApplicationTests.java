218327d54ddfcc4b70e21ae7b94faed1ddf048d9
Oswaldo Neto <oswaldo@Oswaldos-MacBook-Pro.local>
2021-08-07 17:51:59 -0300

    Invillia Reinvent Practice Project - Online Store

---
+++ shopping-cart/src/test/java/com/invillia/reinvent/mom/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.reinvent.mom.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
